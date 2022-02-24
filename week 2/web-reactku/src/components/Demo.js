eabffb0ff0a30041a1067ada8ec5cf00563b37c2
Mochammad Syaifuddin Zuhri <mochammadsyaifuddinz@gmail.com>
2022-02-24 13:03:42 +0700

    add demo style

---
+++ week 2/web-reactku/src/components/Demo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function demo() {
+  return (
+    <div class="container">
+      <div class="card">
+        <img
+          src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1598781485/dlow694gxntbvpvsjmrm.jpg"
+          alt="Avatar"
+        />
+        <div class="container">
+          <h4>
+            <b>Politeknik Negeri Malang</b>
+          </h4>
+          <marquee width="100%" height="40">Jl. Soekarno Hatta No.9, Jatimulyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65141</marquee>
+        </div>
+      </div>
+    </div>
+  );
+}
--- week 2/web-reactku/src/index.js
+++ week 2/web-reactku/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Demo from "./components/Demo";
+import "./styles/demo.css";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const Hello = () => {
-  return <p>Hello</p>;
+  return <p>Hello World</p>;
 };
 
-ReactDOM.render(<Hello />, document.getElementById("root"));
+ReactDOM.render(<Demo />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
